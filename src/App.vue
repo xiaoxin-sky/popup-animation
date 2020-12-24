be7bad0d3ccb61f25c9582d66f319c04d3486478
1912820899 <779931346@qq.com>
2020-12-24 16:12:21 +0800

    完成命令行弹窗功能，下一步实现是否传递元素，来决定弹出位置

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <div ref="btn" class="btn" @click="showPopup"></div>
+    <div ref="btn1" class="btn1" @click="showPopup1"></div>
+  </div>
 </template>
 
 <script>
+import Vue from "vue";
+import pop from "./components/popup";
+Vue.prototype.$pop = pop;
 export default {
   name: "App",
-  components: {}
+  data() {
+    return {};
+  },
+  methods: {
+    showPopup() {
+      this.$pop({
+        msg: "djdj",
+        target: this.$refs.btn
+      });
+    },
+    showPopup1() {
+      this.$pop({
+        msg: "djdj",
+        target: this.$refs.btn1
+      });
+    }
+  }
 };
 </script>
 
@@ -16,6 +38,26 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+body {
+  margin: 0;
+  padding: 0;
+}
+@mixin btn {
+  width: 30px;
+  height: 30px;
+  position: absolute;
+}
+.btn {
+  @include btn();
+  left: 10px;
+  top: 10px;
+  background-color: red;
+}
+.btn1 {
+  @include btn();
+  background-color: hotpink;
+  bottom: 10px;
+  right: 10px;
 }
 </style>
---
+++ src/components/Popup.vue
@@ -0,0 +1,77 @@
+<template>
+  <transition
+    mode="out-in"
+    :css="false"
+    v-on:before-enter="beforeEnter"
+    v-on:enter="enter"
+  >
+    <div v-if="show" class="popup">msg:{{ msg }}</div>
+  </transition>
+</template>
+
+<script>
+export default {
+  name: "myPopup",
+  mounted() {
+    this.show = true;
+  },
+  data() {
+    return {
+      show: false
+    };
+  },
+  methods: {
+    beforeEnter(el) {
+      const { x, y } = this.position;
+      el.style.opacity = 0;
+      el.style.left = x + "px";
+      el.style.top = y + "px";
+      el.style.transform = "translate(-50%,-50%) scale(0)";
+    },
+    enter(el, done) {
+      const { innerHeight, innerWidth } = window;
+      console.log(innerHeight, innerWidth);
+      //初始化弹出位置，从中间弹出
+      el.style.opacity = 1;
+      el.style.left = innerWidth / 2 + "px";
+      el.style.top = innerHeight / 2 + "px";
+      el.style.transition = "0.6s cubic-bezier(.61,.59,.63,1.6)";
+      el.style.transform = "translate(-50%,-50%) scale(1)";
+      el.addEventListener("transitionend", done);
+    },
+    getElementCenterPosition(target) {
+      const {
+        offsetWidth: elementWidth,
+        offsetHeight: elementHeight,
+        offsetLeft: x,
+        offsetTop: y
+      } = target;
+      const centerX = x + elementWidth / 2;
+      const centerY = y + elementHeight / 2;
+      return {
+        x: centerX,
+        y: centerY
+      };
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.popup {
+  position: fixed;
+  width: 200px;
+  height: 200px;
+  background-color: green;
+}
+.v-enter-active {
+  transition-duration: 1s;
+  transition-property: opacity;
+}
+.v-enter {
+  opacity: 0;
+}
+.v-enter-to {
+  opacity: 1;
+}
+</style>
